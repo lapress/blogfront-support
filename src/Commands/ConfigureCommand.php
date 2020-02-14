2581e4725b81e6f1fab6ff539c38e3813837cca4
Sebastian Szczepanski <sebastian@ably.pl>
2020-02-14 22:35:16 +0100

    initial commit

---
+++ src/Commands/ConfigureCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Commands;
+
+use Illuminate\Console\Command;
+
+/**
+ * @author    Sebastian Szczepański
+ * @copyright ably
+ */
+class ConfigureCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'blogfront:configure';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Configure WordPress';
+
+    /**
+     * Create a new command instance.
+     *
+     * @param Filesystem $filesystem
+     */
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $wordpressSampleConfig = wordpress_path('wp-config-sample.php');
+        if ($this->filesystem->exists($wordpressSampleConfig)) {
+            $this->filesystem->delete($wordpressSampleConfig);
+        }
+
+        $this->filesystem->copy(
+            __DIR__.'/stubs/wp-config.php.stub',
+            wordpress_path('wp-config.php')
+        );
+    }
+}
---
+++ src/PackageServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace LaPress\BlogFront;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * @author    Sebastian Szczepański
+ * @copyright ably
+ */
+class PackageServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->commands([
+
+        ]);
+    }
+}
